b9d49a4fa02e823d36eeca12ad907ae2141f2c1b
rolindaz <rolinda.zamboni@gmail.com>
2025-06-18 10:02:05 +0200

    Create folders for today's exercise

---
+++ day-2/1_milestone/assets/js/script.js
@@ -0,0 +1,36 @@
+const endpointUrl = 'https://lanciweb.github.io/demo/api/pictures/';
+const rowEl = document.querySelector('.row');
+
+fetch(endpointUrl)
+.then(res => res.json())
+.then(data => {
+    console.log(data);
+    let rowElHTML = ''
+    data.forEach((obj)=>{
+        const {
+            title,
+            date,
+            url
+        } = data;
+        const photoMarkup = `
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div class="card">
+                            <img id="pin" src="./assets/img/pin.svg" alt="Pin">
+                            <div class="card-head">
+                                <img src=${obj.url} alt="${obj.title}">
+                            </div>
+                            <div class="card-body">
+                                <span>
+                                    ${obj.title}
+                                </span>
+                                <span>
+                                    ${obj.date}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                `;
+        rowElHTML += photoMarkup;
+    });
+    rowEl.insertAdjacentHTML('beforeend', rowElHTML);
+});
